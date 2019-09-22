6e089c53ee52eabae699218e0a96245b1f0a93c7
ong-ar <reclusis@gmail.com>
2019-09-22 21:21:03 +0900

    textinput type 추가 및 소수점 제한 추가

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -6,7 +6,20 @@ class NewTransaction extends StatelessWidget {
 
   final Function _addTx;
 
+  final FocusNode _amountNode = FocusNode();
+
   NewTransaction(this._addTx);
+
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    _addTx(enteredTitle, enteredAmount);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,28 +33,19 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
               controller: titleController,
-              // onChanged: (val) {
-              //   titleInput = val;
-              // },
+              onSubmitted: (_) => _amountNode.requestFocus(),
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
-              // onChanged: (val) {
-              //   amountInput = val;
-              // },
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
+              focusNode: _amountNode,
             ),
             FlatButton(
               child: Text('Add Transation'),
               textColor: Colors.purple,
-              onPressed: () {
-                // print(titleInput);
-                // print(amountInput);
-                print(titleController.text);
-                print(amountController.text);
-                _addTx(
-                    titleController.text, double.parse(amountController.text));
-              },
+              onPressed: submitData,
             )
           ],
         ),
