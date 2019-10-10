6d79131fbb2ee92b92559253bfa26d687a1d005e
ong-ar <reclusis@gmail.com>
2019-10-10 22:00:53 +0900

    styling list, chart

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -12,7 +12,7 @@ class ChartBar extends StatelessWidget {
     return Column(
       children: <Widget>[
         Container(
-          height: 21,
+          height: 20,
           child: FittedBox(
             child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
           ),
